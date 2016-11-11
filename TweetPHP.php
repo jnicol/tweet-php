42f6ee0d159164180da5f8c800265244f719750b
Lee Collings <lee@leecollings.co>
2016-11-11 12:54:24 +0000

    Display rel="nofollow" for all links within tweet
    Added a boolean parameter to show `rel="nofollow"` for all links within tweet.

--- TweetPHP.php
+++ TweetPHP.php
@@ -62,6 +62,7 @@
           'twitter_template'      => '<h2>Latest tweets</h2><ul id="twitter">{tweets}</ul>',
           'tweet_template'        => '<li><span class="status">{tweet}</span> <span class="meta"><a href="{link}">{date}</a></span></li>',
           'error_template'        => '<li><span class="status">Our twitter feed is unavailable right now.</span> <span class="meta"><a href="{link}">Follow us on Twitter</a></span></li>',
+          'nofollow_links'        => false,
           'debug'                 => false
         ),
         $options
@@ -224,6 +225,9 @@
       $output = str_replace('{tweet}', $tweet_text, $this->options['tweet_template']);
       $output = str_replace('{link}', $href, $output);
       $output = str_replace('{date}', $display_time, $output);
+      if ($this->options['nofollow_links']) {
+          $output = str_replace('<a ', '<a rel="nofollow" ', $output);
+      }
 
       return $output;
     }
